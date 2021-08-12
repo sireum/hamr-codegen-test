492450c4a36bc99c1d92e96772dc3a4c26f3ca5e
Jason Belt <belt@ksu.edu>
2021-08-12 19:54:48 +0000

    udpate expected

--- org/sireum/hamr/codegen/test/normal/CodeGenTest_Base.scala
+++ org/sireum/hamr/codegen/test/normal/CodeGenTest_Base.scala
@@ -498,7 +498,7 @@ class CodeGenTest_Base extends CodeGenTest {
     // of 'expected' and 'models' into the test class via the RC macros .  These can then
     // be retrieved as a map from 'exploded path' to 'contents' via a call to 'testResources()'
 
-    return RC.base64(Vector("../../../../../../")) { (p, f) =>
+    RC.base64(Vector("../../../../../../")) { (p, f) =>
       val cname = "CodeGenTest_Base"
       val allowedDirs: ISZ[Predef.String] = ISZ(s"expected/${cname}", s"models/${cname}")
 
