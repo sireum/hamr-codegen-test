9cebe93fe22ed498360fd1c61d35314baf5bf3ce
Jason Belt <belt@ksu.edu>
2022-04-28 18:16:09 +0000

    cleanup

--- scala/org/sireum/hamr/codegen/test/behavior/BaseBehaviorTest.scala
+++ scala/org/sireum/hamr/codegen/test/behavior/BaseBehaviorTest.scala
@@ -9,38 +9,37 @@ class BaseBehaviorTest extends CodegenBehaviorTest{
 
   val modelsDir = TestUtil.getRootDirectory(getClass) / "resources" / "models"
 
+
+  "temp_control_simple_temp" in {
+      val modelDir = modelsDir / "CodeGenTest_Base" / "temp_control_simple_temp"
+      val description = "Temp Control model with temp data structure with 1 field"
+
+      testAir(
+        testName = modelDir.name,
+        testDescription = description,
+        testOptions = baseOptions(
+          slangOutputDir = Some((modelDir / "hamr" / "slang").value),
+          aadlRootDir = Some((modelDir / "aadl").value),
+          verbose = verbose
+        ),
+        // include test modes from super (e.g. maybe from env var)
+        testModes = testModes :+ TestMode.generated_unit_tests
+      )
+  }
+
   val tests = Tests {
 
-//    { // static approach
-//      {
-//        val modelDir = modelsDir / "CodeGenTest_Base" / "building_control_gen_mixed"
-//        val description = ""
-//
-//        test(
-//          testName = modelDir.name,
-//          testDescription = description,
-//          testOptions = baseOptions(
-//            slangOutputDir = Some((modelDir / "hamr" / "slang").value),
-//            aadlRootDir = Some(modelDir.value),
-//            verbose = verbose
-//          ),
-//          // include test modes from super (e.g. maybe from env var)
-//          testModes = testModes :+ TestMode.generated_unit_tests
-//        )
-//      }
-//    }
-
-    { // Temp_control_simple_temp
+    { // static approach
       {
-        val modelDir = modelsDir / "CodeGenTest_Base" / "temp_control_simple_temp"
-        val description = "Temp Control model with temp data structure with 1 field"
+        val modelDir = modelsDir / "CodeGenTest_Base" / "building_control_gen_mixed"
+        val description = ""
 
         test(
           testName = modelDir.name,
           testDescription = description,
           testOptions = baseOptions(
             slangOutputDir = Some((modelDir / "hamr" / "slang").value),
-            aadlRootDir = Some((modelDir / "aadl").value),
+            aadlRootDir = Some(modelDir.value),
             verbose = verbose
           ),
           // include test modes from super (e.g. maybe from env var)
@@ -49,12 +48,12 @@ class BaseBehaviorTest extends CodegenBehaviorTest{
       }
     }
 
-//    { // dynamic approach
-//      var testRoots: ISZ[Os.Path] = locateHamrTestFiles(modelsDir)
-//
-//      for (hamrTestFile <- testRoots) {
-//        genTestFromFile(hamrTestFile)
-//      }
-//    }
+    { // dynamic approach
+      var testRoots: ISZ[Os.Path] = locateHamrTestFiles(modelsDir)
+
+      for (hamrTestFile <- testRoots) {
+        genTestFromFile(hamrTestFile)
+      }
+    }
   }
 }
