894e7b808fb02316a63dc66ef94b1f0bea922676
Jason Belt <belt@ksu.edu>
2022-06-21 14:16:49 -0500

    adapt to phantom cli fix

--- scala/org/sireum/hamr/codegen/test/behavior/BaseBehaviorTest.scala
+++ scala/org/sireum/hamr/codegen/test/behavior/BaseBehaviorTest.scala
@@ -5,30 +5,37 @@ import org.sireum.hamr.codegen.test.CodegenBehaviorTest
 import org.sireum.hamr.codegen.test.util.{TestMode, TestUtil}
 import TestUtil._
 
-class BaseBehaviorTest extends CodegenBehaviorTest{
+class BaseBehaviorTest extends CodegenBehaviorTest {
 
   val modelsDir = TestUtil.getRootDirectory(getClass) / "resources" / "models"
 
 
   "temp_control_simple_temp" in {
-      val modelDir = modelsDir / getClass.getSimpleName / "temp_control_simple_temp"
-      val description = "Temp Control model with temp data structure with 1 field"
-      val packageName: Option[org.sireum.String] = Some("TempControlSimpleTemp")
+    val modelDir = modelsDir / getClass.getSimpleName / "temp_control_simple_temp"
+    val description = "Temp Control model with temp data structure with 1 field"
+    val packageName: Option[org.sireum.String] = Some("TempControlSimpleTemp")
 
-      testAir(
-        testName = modelDir.name,
-        testDescription = description,
-        testOptions = baseOptions(
-          slangOutputDir = Some((modelDir / "hamr" / "slang").value),
-          aadlRootDir = Some((modelDir / "aadl").value),
-          packageName = packageName,
-          verbose = verbose
-        ),
-        phantomOptions = None(),
-        logikaOptions = logikaOptions,
-        // include test modes from super (e.g. maybe from env var)
-        testModes = testModes :+ TestMode.generated_unit_tests
-      )
+    // there are multiple system implementations in this project so need to
+    // help phantom out if it's used (e.g. CI reg testing)
+    val phantomOps: ISZ[String] = ISZ(
+      "--main-package", "./packages/TempControlSoftwareSystem.aadl",
+      "--sys-impl", "TempControlSoftwareSystem.i",
+      "--projects", ".")
+
+    testAir(
+      testName = modelDir.name,
+      testDescription = description,
+      testOptions = baseOptions(
+        slangOutputDir = Some((modelDir / "hamr" / "slang").value),
+        aadlRootDir = Some((modelDir / "aadl").value),
+        packageName = packageName,
+        verbose = verbose
+      ),
+      phantomOptions = Some(st"${(phantomOps, " ")}".render),
+      logikaOptions = logikaOptions,
+      // include test modes from super (e.g. maybe from env var)
+      testModes = testModes :+ TestMode.generated_unit_tests
+    )
   }
 
   val tests = Tests {
