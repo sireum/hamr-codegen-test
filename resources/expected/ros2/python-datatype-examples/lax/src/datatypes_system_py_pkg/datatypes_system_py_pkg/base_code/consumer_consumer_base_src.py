e162e50cb0881148aa5aab6f1dd495ee32fcc0c7
Cat <cat@santoslab.org>
2025-03-07 15:10:33 -0600

    Python Launch Files

---
+++ resources/expected/ros2/python-datatype-examples/lax/src/datatypes_system_py_pkg/datatypes_system_py_pkg/base_code/consumer_consumer_base_src.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from queue import Queue
+from rclpy.callback_groups import ReentrantCallbackGroup
+from datatypes_system_py_pkg_interfaces.msg import Boolean
+from datatypes_system_py_pkg_interfaces.msg import Integer64
+from datatypes_system_py_pkg_interfaces.msg import Float64
+from datatypes_system_py_pkg_interfaces.msg import Character
+from datatypes_system_py_pkg_interfaces.msg import String
+from datatypes_system_py_pkg_interfaces.msg import Integer8
+from datatypes_system_py_pkg_interfaces.msg import Integer16
+from datatypes_system_py_pkg_interfaces.msg import Integer32
+from datatypes_system_py_pkg_interfaces.msg import Unsigned8
+from datatypes_system_py_pkg_interfaces.msg import Unsigned16
+from datatypes_system_py_pkg_interfaces.msg import Unsigned32
+from datatypes_system_py_pkg_interfaces.msg import Unsigned64
+from datatypes_system_py_pkg_interfaces.msg import Float32
+from datatypes_system_py_pkg_interfaces.msg import MyEnum
+from datatypes_system_py_pkg_interfaces.msg import MyStructi
+from datatypes_system_py_pkg_interfaces.msg import MyArrayOneDim
+from datatypes_system_py_pkg_interfaces.msg import MyArrayUnbounded
+from datatypes_system_py_pkg_interfaces.msg import MyArrayTwoDim
+
+#========================================================
+# Re-running Codegen will overwrite changes to this file
+#========================================================
+
+class consumer_consumer_base(Node):
+    def __init__(self):
+        super().__init__("consumer_consumer")
+
+        self.cb_group_ = ReentrantCallbackGroup()
+
+        # Setting up connections
+        self.consumer_consumer_myBoolean_subscription_ = self.create_subscription(
+            Boolean,
+            "consumer_consumer_myBoolean",
+            self.handle_myBoolean,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myInteger_subscription_ = self.create_subscription(
+            Integer64,
+            "consumer_consumer_myInteger",
+            self.handle_myInteger,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myFloat_subscription_ = self.create_subscription(
+            Float64,
+            "consumer_consumer_myFloat",
+            self.handle_myFloat,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myCharacter_subscription_ = self.create_subscription(
+            Character,
+            "consumer_consumer_myCharacter",
+            self.handle_myCharacter,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myString_subscription_ = self.create_subscription(
+            String,
+            "consumer_consumer_myString",
+            self.handle_myString,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myInt8_subscription_ = self.create_subscription(
+            Integer8,
+            "consumer_consumer_myInt8",
+            self.handle_myInt8,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myInt16_subscription_ = self.create_subscription(
+            Integer16,
+            "consumer_consumer_myInt16",
+            self.handle_myInt16,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myInt32_subscription_ = self.create_subscription(
+            Integer32,
+            "consumer_consumer_myInt32",
+            self.handle_myInt32,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myInt64_subscription_ = self.create_subscription(
+            Integer64,
+            "consumer_consumer_myInt64",
+            self.handle_myInt64,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myUInt8_subscription_ = self.create_subscription(
+            Unsigned8,
+            "consumer_consumer_myUInt8",
+            self.handle_myUInt8,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myUInt16_subscription_ = self.create_subscription(
+            Unsigned16,
+            "consumer_consumer_myUInt16",
+            self.handle_myUInt16,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myUInt32_subscription_ = self.create_subscription(
+            Unsigned32,
+            "consumer_consumer_myUInt32",
+            self.handle_myUInt32,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myUInt64_subscription_ = self.create_subscription(
+            Unsigned64,
+            "consumer_consumer_myUInt64",
+            self.handle_myUInt64,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myFloat32_subscription_ = self.create_subscription(
+            Float32,
+            "consumer_consumer_myFloat32",
+            self.handle_myFloat32,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myFloat64_subscription_ = self.create_subscription(
+            Float64,
+            "consumer_consumer_myFloat64",
+            self.handle_myFloat64,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myEnum_subscription_ = self.create_subscription(
+            MyEnum,
+            "consumer_consumer_myEnum",
+            self.handle_myEnum,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myStruct_subscription_ = self.create_subscription(
+            MyStructi,
+            "consumer_consumer_myStruct",
+            self.handle_myStruct,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myArray1_subscription_ = self.create_subscription(
+            MyArrayOneDim,
+            "consumer_consumer_myArray1",
+            self.handle_myArray1,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myArray2_subscription_ = self.create_subscription(
+            MyArrayUnbounded,
+            "consumer_consumer_myArray2",
+            self.handle_myArray2,
+            1,
+            callback_group=self.cb_group_)
+
+        self.consumer_consumer_myArray3_subscription_ = self.create_subscription(
+            MyArrayTwoDim,
+            "consumer_consumer_myArray3",
+            self.handle_myArray3,
+            1,
+            callback_group=self.cb_group_)
---
+++ resources/expected/ros2/python-datatype-examples/lax/src/datatypes_system_py_pkg/datatypes_system_py_pkg/user_code/consumer_consumer_src.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from datatypes_system_py_pkg.base_code.consumer_consumer_runner import consumer_consumer
+from datatypes_system_py_pkg.base_code.enum_converter import *
+
+#===========================================================
+# This file will not be overwritten when re-running Codegen
+#===========================================================
+
+#=================================================
+#  I n i t i a l i z e    E n t r y    P o i n t
+#=================================================
+def initialize(n):
+    node = consumer_consumer()
+    node.data = n
+    node.get_logger().info("Initialize Entry Point invoked")
+
+    # Initialize the node
+
+#=================================================
+#  C o m p u t e    E n t r y    P o i n t
+#=================================================
+def handle_myBoolean(msg):
+    pass # Handle myBoolean msg
+
+def handle_myInteger(msg):
+    pass # Handle myInteger msg
+
+def handle_myFloat(msg):
+    pass # Handle myFloat msg
+
+def handle_myCharacter(msg):
+    pass # Handle myCharacter msg
+
+def handle_myString(msg):
+    pass # Handle myString msg
+
+def handle_myInt8(msg):
+    pass # Handle myInt8 msg
+
+def handle_myInt16(msg):
+    pass # Handle myInt16 msg
+
+def handle_myInt32(msg):
+    pass # Handle myInt32 msg
+
+def handle_myInt64(msg):
+    pass # Handle myInt64 msg
+
+def handle_myUInt8(msg):
+    pass # Handle myUInt8 msg
+
+def handle_myUInt16(msg):
+    pass # Handle myUInt16 msg
+
+def handle_myUInt32(msg):
+    pass # Handle myUInt32 msg
+
+def handle_myUInt64(msg):
+    pass # Handle myUInt64 msg
+
+def handle_myFloat32(msg):
+    pass # Handle myFloat32 msg
+
+def handle_myFloat64(msg):
+    pass # Handle myFloat64 msg
+
+def handle_myEnum(msg):
+    pass # Handle myEnum msg
+
+def handle_myStruct(msg):
+    pass # Handle myStruct msg
+
+def handle_myArray1(msg):
+    pass # Handle myArray1 msg
+
+def handle_myArray2(msg):
+    pass # Handle myArray2 msg
+
+def handle_myArray3(msg):
+    pass # Handle myArray3 msg
+
